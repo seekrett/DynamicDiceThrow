118d0dd279a9570e9aae0215f55d024497ce0cad
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-26 15:57:53 -0400

    implemented dynamic ui

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,10 +1,12 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 
 
 /*
@@ -23,10 +25,35 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val dieViewModel : DieViewModel by lazy {
+            ViewModelProvider(this)[DieViewModel::class.java]
+        }
+
+        val isPortrait = findViewById<View>(R.id.container2) == null
+
+        // retain state
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container1, ButtonFragment())
+                .addToBackStack(null)
+                .setReorderingAllowed(true)
+                .commit()
+        }
+
+        // if landscape
+        if (!isPortrait) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container2, DieFragment())
+                .commit()
+        }
+
         /* TODO 1: Load fragment(s)
             - Show _only_ ButtonFragment if portrait
             - show _both_ fragments if Landscape
           */
+
     }
 
     /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
@@ -35,7 +62,15 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
     // This callback function gets invoked when the child Fragment invokes it
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
+        val isPortrait = findViewById<View>(R.id.container2) == null
 
+        if (isPortrait) {
+            supportFragmentManager
+                .beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.container1, DieFragment())
+                .commit()
+        }
     }
 
 
